492313abc3063825b61629ad8b54cd499efae5f1
MohammadReza PourMohammad <mrpm.root@gmail.com>
2025-12-13 10:14:29 +0330

    add language strings

---
+++ lang/en/webservice_mcp.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language strings for the MCP web service plugin.
+ *
+ * @package     webservice_mcp
+ * @author      MohammadReza PourMohammad <onbirdev@gmail.com>
+ * @copyright   2025 MohammadReza PourMohammad
+ * @link        https://onbir.dev
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['err_empty_request'] = 'Request body is empty';
+$string['err_invalid_json'] = 'Invalid JSON: {$a}';
+$string['err_invalid_jsonrpc'] = 'Invalid JSON-RPC version: {$a}';
+$string['err_invalid_params'] = 'Invalid parameters: {$a}';
+$string['err_invalid_request'] = 'Invalid request: {$a}';
+$string['err_missing_method'] = 'Missing method: {$a}';
+$string['err_missing_tool_name'] = 'Missing tool name: {$a}';
+$string['mcp:use'] = 'Use MCP web service';
+$string['pluginname'] = 'Model Context Protocol';
+$string['privacy:metadata'] = 'The MCP web service plugin does not store any personal data. It provides a protocol for accessing existing Moodle web service functions.';
