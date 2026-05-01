70a209d9b450cc7644fc2e76c8b49ce5c98fedd5
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-05-01 19:43:11 +0100

    chore(ci): completely streamline and fix the release pipeline
    This deletes the overinflated and redundant delivery and main-release-tag workflows. The release pipeline is now a single, elegant workflow that triggers directly on pushes to main. It also squashes all Node 20 deprecation warnings.

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026042203;
+$plugin->version = 2026042204;
 $plugin->requires = 2023041800;
 $plugin->component = 'webservice_mcp';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.7.3';
+$plugin->release = '0.7.4';
