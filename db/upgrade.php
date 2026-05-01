cf81b6a51c4b26ed58896ca5ff08499df34ea09d
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-05-02 00:32:32 +0100

    feat(10-01): add webservice_mcp_memory table and upgrade step

--- db/upgrade.php
+++ db/upgrade.php
@@ -188,5 +188,26 @@ function xmldb_webservice_mcp_upgrade(int $oldversion): bool {
         upgrade_plugin_savepoint(true, 2026042201, 'webservice', 'mcp');
     }
 
+    if ($oldversion < 2026050100) {
+        $table = new xmldb_table('webservice_mcp_memory');
+
+        if (!$dbman->table_exists($table)) {
+            $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+            $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+            $table->add_field('content', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+            $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+            $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+            $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+            $table->add_key('userid_fk', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+
+            $table->add_index('userid_idx', XMLDB_INDEX_NOTUNIQUE, ['userid']);
+
+            $dbman->create_table($table);
+        }
+
+        upgrade_plugin_savepoint(true, 2026050100, 'webservice', 'mcp');
+    }
+
     return true;
 }
